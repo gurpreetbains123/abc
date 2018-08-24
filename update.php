5a07bc3c70fa64c8a92f6352a096f33a00f7f997
gurpreet <gurpreet@rudrainnovatives.com>
2018-08-24 13:01:47 +0530

    php

--- update.php
+++ update.php
@@ -1,6 +1,7 @@
 <?php
 
 #echo shell_exec('cd /var/www');
-echo shell_exec('git pull');
- ?>
+#echo shell_exec('git pull');
+echo shell_exec("cd /var/www/html/gitautodev  && /full/path/to/bin/git pull 2>&1"); 
+?>
 ~   
